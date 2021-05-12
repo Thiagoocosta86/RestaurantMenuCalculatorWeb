7e1e7d0b2fc1d33bccdb1e3bb0bcb282757673ec
Thiagoocosta86 <74249031+Thiagoocosta86@users.noreply.github.com>
2021-05-12 01:14:17 +0100

    ...

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-function CheckPassword(inputtxt){
+function CheckPassword(inputtxt) {
 
     var passwor = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
-    if (inputtxt.value.match(passwor))
-    {
+    if (inputtxt.value.match(passwor)) {
         alert('Strong password!')
         return true;
-    } else
-    {
+    } else {
         alert('Too weak! Password must be 8 to 15 characters and contain at least one numeric digit, one lowercase and uppercase letter, and one special character')
         return false;
     }
@@ -15,33 +13,33 @@ function CheckPassword(inputtxt){
 
 document.getElementById('getRandomUser').addEventListener('click', getRandomUser);
 
-function getRandomUser(){
+function getRandomUser() {
     fetch('https://randomuser.me/api/?results=5')
-        .then((res)=>res.json())
-        .then((data)=>{
+        .then((res) => res.json())
+        .then((data) => {
             let isso = data.results;
             let output = '<h2>Restaurant Customers</h2>'
             console.log(data);
-            isso.forEach(function (user){
-                output +=`
+            isso.forEach(function (user) {
+                output += `
                 <div class="customer">
                                 
                     <a target="_blank" href="${user.picture.thumbnail}">
                     <img src="${user.picture.thumbnail}" alt="User">
                     </a>
                                 
-                 <ul>
+                 <ul><b>
                 <li>${user.name.first} ${user.name.last}</i>
                     <li>Age: ${user.dob.age} Gender: ${user.gender}</li>
                     <li>Email: ${user.email}</li>
                     <li>City: ${user.location.city}</li>
-                    
+                 </ul>   
                                
                 </div>
                 
                 `;
-                
+
             });
-            document.getElementById('output').innerHTML=output;
+            document.getElementById('output').innerHTML = output;
         })
 }
