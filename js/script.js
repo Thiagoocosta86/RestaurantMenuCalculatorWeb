587f4dbbc7cf8c6ab53069a23a6e2a3570088e5e
Thiagoocosta86 <74249031+Thiagoocosta86@users.noreply.github.com>
2021-05-18 10:22:43 +0100

    improving password checker.

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@ function CheckPassword(inputtxt) {
 
     var passwor = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
     if (inputtxt.value.match(passwor)) {
-        alert('Strong password!')
+        document.getElementById('inValid').style.visibility = "hidden";
+        document.getElementById('Valid').style.visibility = "visible";
         return true;
     } else {
-        alert('Too weak! Password must be 8 to 15 characters and contain at least one numeric digit, one lowercase and uppercase letter, and one special character')
+        document.getElementById('Valid').style.visibility = "hidden";
+        document.getElementById('inValid').style.visibility = "visible";
         return false;
     }
 
