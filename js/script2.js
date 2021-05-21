879ed8d1df88f931aeb4ca95e797b5602448367c
Thiagoocosta86 <74249031+Thiagoocosta86@users.noreply.github.com>
2021-05-21 03:48:52 +0100

    using v-binding do create a alert message once hover the mouse on the menu item.

--- js/script2.js
+++ js/script2.js
@@ -1,2 +1,24 @@
 d3.select('h3').style('color','chocolate');
 d3.select('h3').style('font-size', '25px');
+
+var app1 = new Vue({
+    el: '#app-1', 
+            
+    data: {
+        message: "Check for allergens when you order"
+    }
+});
+var app2 = new Vue({
+    el: '#app-2', 
+            
+    data: {
+        message: "Check for allergens when you order"
+    }
+});
+var app3 = new Vue({
+    el: '#app-3', 
+            
+    data: {
+        message: "Check for allergens when you order"
+    }
+});
